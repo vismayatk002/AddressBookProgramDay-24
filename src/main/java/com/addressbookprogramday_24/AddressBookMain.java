150066a3b2c3d2adb5cbbc45725dae58d52dab7b
vismayatk002 <vismayatk002@gmail.com>
2021-08-26 22:22:51 +0530

    Added welcome message

---
+++ src/main/java/com/addressbookprogramday_24/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.addressbookprogramday_24;
+
+public class AddressBookMain {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "Welcome to Address Book Program" );
+    }
+}
