c17ae19b4006efe45746c8993ab68c2813b693d5
vismayatk002 <vismayatk002@gmail.com>
2021-08-26 22:53:24 +0530

    Refactor Address book program UC1 to UC10

---
+++ src/main/java/com/addressbookprogramday_24/AddressBook.java
@@ -0,0 +1,40 @@
+package com.addressbookprogramday_24;
+
+public class AddressBook extends ContactDetails{
+
+    String address, city, state;
+    int zip;
+    long phoneNo;
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+    public String getState(){
+        return state;
+    }
+    public void setPhoneNo(long phoneNo){
+        this.phoneNo = phoneNo;
+    }
+    public long getPhoneNo(){
+        return phoneNo;
+    }
+    public void setZip(int zip){
+        this.zip = zip;
+    }
+    public int getZip(){
+        return zip;
+    }
+}
+
---
+++ src/main/java/com/addressbookprogramday_24/OperateContact.java
@@ -0,0 +1,93 @@
+package com.addressbookprogramday_24;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.List;
+
+public class OperateContact{
+
+    HashMap<String, AddressBook> contactMap = new HashMap<>();
+   
+    public void editContact(){
+
+        String address;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for update Address : ");
+        String editName = sc.nextLine();
+        int flag = 1;
+        for (String firstName : contactMap.keySet())
+        {
+            AddressBook contact = contactMap.get(firstName);
+
+            if(editName.equals(contact.getFirstName())){
+                System.out.print("\nEnter your new Address : ");
+                address = sc.nextLine();
+                contact.setAddress(address);
+                flag = 0;
+            }
+        }
+        if(flag == 1){
+            System.out.print("\ncouldn't find the contact..");
+        }
+        else{
+            System.out.print("\nYour contact updated.");
+        }
+    }
+    public void deleteContact(){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for delete : ");
+        String deleteName = sc.nextLine();
+        int flag = 1;
+        for (String firstName : contactMap.keySet())
+        {
+            AddressBook contact = contactMap.get(firstName);
+            if(deleteName.equals(contact.getFirstName())){
+                contactMap.remove(firstName);
+                flag = 0;
+            }
+        }
+        if(flag == 1){
+            System.out.print("\ncouldn't find the contact..");
+        } 
+        else{
+            System.out.print("\nYour contact deleted..");
+        }
+        sc.close();   
+    }
+    public void showContact(){
+
+        for (String firstName : contactMap.keySet())
+        {
+            AddressBook contact = contactMap.get(firstName);
+
+            System.out.print("\n-----------------");
+            System.out.print("\nFirst Name  : " +  contact.getFirstName());
+            System.out.print("\nLast Name   : " +  contact.getLastName());
+            System.out.print("\nAddress     : " +  contact.getAddress());
+            System.out.print("\nCity        : " +  contact.getCity());
+            System.out.print("\nState       : " +  contact.getState());
+            System.out.print("\nPone Number : " +  contact.getPhoneNo());
+            System.out.print("\nE-mail      : " +  contact.getEmail());
+            System.out.print("\nZip         : " +  contact.getZip());
+        }
+    }
+    public void storeContact(String firstName, AddressBook contact){
+
+        this.contactMap.put(firstName, contact);
+    }
+    public void searchPerson() {
+    	 Scanner sc= new Scanner(System.in);
+         System.out.print("\nEnter the city : ");
+         String city = sc.nextLine();
+         
+         List<String> person = contactMap.entrySet() 
+		         .stream() 
+		         .filter(HashMap -> HashMap.getValue().getCity().equals(city))
+		         .map(HashMap -> HashMap.getValue().getFirstName())
+		         .collect(Collectors.toList());
+         
+         System.out.println(String.join(",", person));  
+    }
+}
